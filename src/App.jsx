36cf1d6818e0d2cf42e88ff3db2b9dbfd0519ebb
Raishawn Jones <141539039+LLTS1022@users.noreply.github.com>
2025-06-22 13:24:52 -0500

    first

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useRef, useState, useEffect } from "react";
 import "./App.css";
-import Lottie from "@lottielab/lottie-player/react"
+import Lottie from "@lottielab/lottie-player/react";
 
 export default function FocusInputLottie() {
   const animationRef = useRef(null);
@@ -8,30 +8,45 @@ export default function FocusInputLottie() {
   const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
 
-  const switchState = (stateName) =>{
-    animationRef.current?.interactivity?.goToState(stateName, {duration: 0.1})
+  const switchState = (stateName) => {
+    animationRef.current?.interactivity?.goToState(stateName, {
+      duration: 0.1,
+    });
   };
-  const handleUserNameFocus = () => {switchState("Following")};
-  const handlePasswordFocus = () => {switchState(showPassword ? "Peeking" : "Covering")};
-  const handleBlur = () => {switchState("Blinking")} 
+  const handleUserNameFocus = () => {
+    switchState("Following");
+  };
+  const handlePasswordFocus = () => {
+    switchState(showPassword ? "Peeking" : "Covering");
+  };
+  const handleBlur = () => {
+    switchState("Blinking");
+  };
 
-  useEffect(()=>{
-    animationRef.current?.interactivity?.inputs.set('name_length', Math.min(userName.length / 32, 1))
-  }, [userName])
+  useEffect(() => {
+    animationRef.current?.interactivity?.inputs.set(
+      "name_length",
+      Math.min(userName.length / 32, 1)
+    );
+  }, [userName]);
 
-  useEffect(()=>{
-    switchState(showPassword ? "Peeking" : "Covering")
-  }, [showPassword])
+  useEffect(() => {
+    switchState(showPassword ? "Peeking" : "Covering");
+  }, [showPassword]);
 
   return (
     <div className="login-container">
-      <Lottie src="https://cdn.lottielab.com/l/CvhgB3ExbfUqUH.json" ref={animationRef}/>
-      <div className='details'>
-        <div className='detail-label'>
-          <label className='detail-name'>Email</label>
+      <Lottie
+        src="https://cdn.lottielab.com/l/CvhgB3ExbfUqUH.json"
+        ref={animationRef}
+      />
+      <div className="details">
+        <div className="detail-label">
+          <label className="detail-name">Email</label>
         </div>
 
-        <input type="text"
+        <input
+          type="text"
           placeholder="Username"
           className="login-input"
           value={userName}
@@ -40,10 +55,15 @@ export default function FocusInputLottie() {
           onChange={(e) => setUserName(e.target.value)}
         />
 
-        <div className='detail-label'>
-          <label className='detail-name'>Password</label>
+        <div className="detail-label">
+          <label className="detail-name">Password</label>
           <label className="show-password">
-            <input type="checkbox" checked={showPassword} onChange={() => setShowPassword(!showPassword)} /> Show
+            <input
+              type="checkbox"
+              checked={showPassword}
+              onChange={() => setShowPassword(!showPassword)}
+            />{" "}
+            Show Password
           </label>
         </div>
 
